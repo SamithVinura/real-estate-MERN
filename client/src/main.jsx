f5937fd286b0b922284f9db47762a0218c072bba
Samith Vinura <samithvinurakck119@gmail.com>
2024-10-02 13:13:58 +0530

    header created

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,12 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { BrowserRouter, createBrowserRouter, Outlet, Route, RouterProvider, Routes } from 'react-router-dom';
 import Home from '../src/pages/Home.jsx'
 import SignIn from '../src/pages/SignIn.jsx'
 import SignOut from '../src/pages/SignOut.jsx'
 import Profile from '../src/pages/Profile.jsx'
 import About from '../src/pages/About.jsx'
+import Header from '../src/components/Header.jsx';
 const router = createBrowserRouter([
   {
     path: "/",
@@ -32,6 +33,17 @@ const router = createBrowserRouter([
 ]);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+     <BrowserRouter>
+      <Header />
+      <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/about" element={<About />}/>
+      <Route path="/profile" element={<Profile />}/>
+      <Route path="/sign-in" element={<SignIn />}/>  
+       <Route path="/sign-out" element={<SignOut />}/>  
+      
+    </Routes>
+    </BrowserRouter>
+    
   </StrictMode>,
 )
