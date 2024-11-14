ff397fbcf83473240508c2a528b2afa46a934f50
Samith Vinura <samithvinurakck119@gmail.com>
2024-11-14 10:35:28 +0530

    create user update api

---
+++ api/utils/verifyUser.js
@@ -0,0 +1,22 @@
+import { errorHandler } from "./error";
+import jwt from "jsonwebtoken";
+export const verifyToken = (req, res, next) => {
+  const token = req.cookies.access_token;
+  if (!token) {
+    next(errorHandler("401", "unauthorixed"));
+  }
+  try {
+    jwt.verify(
+      token,
+      process.env.JWT_SECRET,
+      (err,
+      (user) => {
+        if (err) {
+          next(errorHandler("403", "Forbidden"));
+        }
+        req.user = user;
+        next();
+      })
+    );
+  } catch (error) {}
+};
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { useSelector } from "react-redux";
 
 const Profile = () => {
   const { currentUser } = useSelector((state) => state.user);
+  const fileRef = useRef(null)
+  const [formData,setFormData] = useState({})
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
 
       <form className="flex flex-col gap-4">
+        <input type="file" ref={fileRef} hidden accept="image/*"/>
         <img
           src={currentUser.avatar}
+          onClick={()=>fileRef.current.click()}
           alt="profile"
           className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
         />
