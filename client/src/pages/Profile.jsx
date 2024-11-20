c247b3d8729f9b4264f42acfb8b150b54689d8c5
Samith Vinura <samithvinurakck119@gmail.com>
2024-11-20 10:00:10 +0530

    complete show user listigs functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -11,13 +11,15 @@ import {
   updateUserStart,
   updateUserSuccess,
 } from "../redux/user/userSlice";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const Profile = () => {
   const dispatch = useDispatch();
   const { currentUser, loading, error } = useSelector((state) => state.user);
   const [formData, setFormData] = useState({});
   const [updateSuccess, setupdateSuccess] = useState(false);
+  const [showListingsError, setShowListingsError] = useState(false);
+  const [listings, setListings] = useState([]);
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
@@ -48,7 +50,7 @@ const Profile = () => {
     try {
       dispatch(deleteUserStart());
       const res = await fetch(`/api/user/delete/${currentUser._id}`, {
-        method: "DELETE"
+        method: "DELETE",
       });
       const data = await res.json();
       if (data?.success === false) {
@@ -56,7 +58,6 @@ const Profile = () => {
         return;
       }
       dispatch(deleteUserSuccess(data));
-      
     } catch (error) {
       dispatch(deleteUserFailure(error.message));
     }
@@ -72,11 +73,26 @@ const Profile = () => {
         return;
       }
       dispatch(signOutUserSuccess(data));
-      
     } catch (error) {
       dispatch(signOutUserFailure(error.message));
     }
   };
+
+  const handleShowListings = async () => {
+    try {
+      setShowListingsError(false);
+      const res = await fetch(`/api/user/listings/${currentUser._id}`);
+      const data = await res.json();
+      if (data?.success === false) {
+        setShowListingsError(true);
+        return;
+      }
+      setListings(data);
+    } catch (error) {
+      setShowListingsError(true);
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -109,19 +125,19 @@ const Profile = () => {
           id="password"
           className="border p-3 rounded-lg"
         />
-     
+
         <button
           className="bg-slate-700 text-white rounded-lg p-3 hover:opacity-95 disabled:opacity-85"
           disabled={loading}
         >
           {loading ? "Loading..." : "Update"}
         </button>
-           <Link to={"/create-listing"}
-            className="bg-green-700 text-white rounded-lg p-3 uppercase text-center hover:opacity-95 disabled:opacity-85"
-           >
-        
+        <Link
+          to={"/create-listing"}
+          className="bg-green-700 text-white rounded-lg p-3 uppercase text-center hover:opacity-95 disabled:opacity-85"
+        >
           Create Listing
-       </Link>
+        </Link>
       </form>
       <div className="flex justify-between mt-5">
         <span
@@ -130,12 +146,51 @@ const Profile = () => {
         >
           Delete Account
         </span>
-        <span className="text-red-700 cursor-pointer" onClick={handleSignOut}>Sign Out</span>
+        <span className="text-red-700 cursor-pointer" onClick={handleSignOut}>
+          Sign Out
+        </span>
       </div>
       <p className="text-green-700 mt-5">
         {updateSuccess ? "User updated successfully !" : ""}
       </p>
       <p className="text-red-700 mt-5">{error ? error : ""}</p>
+      <button className="text-green-700 w-full " onClick={handleShowListings}>
+        Show Listings
+      </button>
+      <p className="text-red-700 mt-5">
+        {showListingsError ? "Error showing listings" : ""}
+      </p>
+      {listings && listings.length > 0 && (
+        <div className="flex flex-col gap-4">
+          <h1 className="text-center mt-7 text-2xl font-semibold">
+            Your Listings
+          </h1>
+          {listings.map((listing) => (
+            <div
+              key={listing._id}
+              className="border rounded-lg p-3 flex justify-between items-center gap-4"
+            >
+              <Link to={`/listing/${listing._id}`}>
+                <img
+                  src={listing?.imageUrls}
+                  alt="listing image"
+                  className="h-16 w-16 object-contain rounded-lg"
+                />
+              </Link>
+              <Link
+                className="flex-1 text-slate-700 font-semibold hover:underline truncate"
+                to={`/listing/${listing._id}`}
+              >
+                <p>{listing.name}</p>
+              </Link>
+              <div className="flex flex-col items-center">
+                <button className="text-red-700 uppercase">Delete</button>
+                <button className="text-green-700 uppercase">Edit</button>
+              </div>
+            </div>
+          ))}{" "}
+        </div>
+      )}
     </div>
   );
 };
