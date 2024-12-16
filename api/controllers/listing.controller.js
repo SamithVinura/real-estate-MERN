2783c54111f8a0f8347d6d1562a423ebef32c0ca
Samith Vinura <samithvinurakck119@gmail.com>
2024-12-16 18:10:53 +0530

    fixed bug in delete listing api

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -12,17 +12,20 @@ export const createListing = async (req, res, next) => {
 
 export const deleteListing = async (req, res, next) => {
   const listing = await Listing.findById(req.params.id);
+
   if (!listing) {
     return next(errorHandler(404, "Listing not found!"));
   }
-  if (req.user.id !== listing.userRef.toString()) {
-    next(errorHandler("401", "You can only delete your own listings!"));
-  } else {
-    try {
-      await Listing.findByIdAndDelete(req.params.id);
-    } catch (error) {
-      next(error);
-    }
+
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, "You can only delete your own listings!"));
+  }
+
+  try {
+    await Listing.findByIdAndDelete(req.params.id);
+    res.status(200).json("Listing has been deleted!");
+  } catch (error) {
+    next(error);
   }
 };
 
